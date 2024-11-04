fb3eadb4e6448349a7a2fc73469698fa30dd9338
kawaiirockcrab <134618865+taehotaeho@users.noreply.github.com>
2024-11-04 14:14:02 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 184 ms, Memory: 17744 KB -BaekjoonHub

---
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] a = {1,1,2,2,2,8};
+        Scanner sc = new Scanner(System.in);
+        for (int i=0; i<6; i++){
+            int temp = sc.nextInt();
+            int result = a[i] - temp;
+            System.out.println(result);
+        }
+    }
+}
