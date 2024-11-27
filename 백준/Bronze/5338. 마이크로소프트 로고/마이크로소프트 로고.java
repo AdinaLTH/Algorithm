848d9225c070853750890dd473ea491653dc5505
kawaiirockcrab <134618865+taehotaeho@users.noreply.github.com>
2024-11-27 11:12:59 +0900

    [Bronze V] Title: 마이크로소프트 로고, Time: 92 ms, Memory: 13836 KB -BaekjoonHub

--- 백준/Bronze/5338. 마이크로소프트 로고/마이크로소프트 로고.java
+++ 백준/Bronze/5338. 마이크로소프트 로고/마이크로소프트 로고.java
@@ -1,9 +1,5 @@
-import java.util.Scanner;
-
 public class Main {
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-
+    public static void main(String[] args) {
         System.out.println("       _.-;;-._");
         System.out.println("'-..-'|   ||   |");
         System.out.println("'-..-'|_.-;;-._|");
