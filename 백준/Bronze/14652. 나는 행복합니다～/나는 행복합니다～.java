267af16d34dfbb0d93deda0efa803be0782bbbaf
kawaiirockcrab <134618865+taehotaeho@users.noreply.github.com>
2025-01-06 23:38:53 +0900

    [Bronze IV] Title: 나는 행복합니다~, Time: 120 ms, Memory: 16044 KB -BaekjoonHub

--- 백준/Bronze/14652. 나는 행복합니다～/나는 행복합니다～.java
+++ 백준/Bronze/14652. 나는 행복합니다～/나는 행복합니다～.java
@@ -3,25 +3,17 @@ import java.util.StringTokenizer;
 
 public class Main {
     
-    private static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		private static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out)); 
     
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int row = Integer.parseInt(st.nextToken());
-        int column = Integer.parseInt(st.nextToken());
-        int mysit = Integer.parseInt(st.nextToken());    
-        int count = 0;
-        
-        for(int i=0;i<row;i++) {
-        	for(int j=0;j<column;j++) {
-        	    if(mysit==count) {
-                        bw.write(i+" "+j);
-                        bw.flush();
-                    }
-                count++;
-        	}
-        }
-        bw.close();
-    }
+	    public static void main(String[] args) throws IOException {
+	        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	        StringTokenizer st = new StringTokenizer(br.readLine());
+	        int N = Integer.parseInt(st.nextToken());
+	        int M = Integer.parseInt(st.nextToken());
+	        int K = Integer.parseInt(st.nextToken());
+	        
+	        bw.write(String.valueOf((K / M)+" "+(K - (M * (K / M)))));
+	        bw.flush();
+	        bw.close();
+	    }
 }
