d767bffa81524de8a8213b25acd85a325c3d67c5
kawaiirockcrab <134618865+taehotaeho@users.noreply.github.com>
2025-01-08 20:03:24 +0900

    [Bronze III] Title: 직사각형에서 탈출, Time: 104 ms, Memory: 14188 KB -BaekjoonHub

--- 백준/Bronze/1085. 직사각형에서 탈출/직사각형에서 탈출.java
+++ 백준/Bronze/1085. 직사각형에서 탈출/직사각형에서 탈출.java
@@ -1,36 +1,20 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.StringTokenizer;
 
 public class Main {
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int x = sc.nextInt();
-        int y = sc.nextInt();
-        int w = sc.nextInt();
-        int h = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
-        int a = w-x;
-        int b = x;
-        int c = h-y;
-        int d = y;
-        int t1 = 0;
-        int t2 = 0;
-        if (a >= b) {
-            t1 = b;
-        } else if(b >= a) {
-            t1 = a;
-        }
+        int x = Integer.parseInt(st.nextToken());
+        int y = Integer.parseInt(st.nextToken());
+        int w = Integer.parseInt(st.nextToken());
+        int h = Integer.parseInt(st.nextToken());
 
-        if (c >= d) {
-            t2 = d;
-        } else if(d >= c) {
-            t2 = c;
-        }
+        int x_min = Math.min(x, w-x);
+        int y_min = Math.min(y, h-y);
 
-        if (t1 >= t2) {
-            System.out.println(t2);
-        } else if(t2 >= t1) {
-            System.out.println(t1);
-        }
+        System.out.println(Math.min(x_min, y_min));
     }
 }
