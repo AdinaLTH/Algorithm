26822acff9842f695886010e336ab5937475f6dc
kawaiirockcrab <134618865+taehotaeho@users.noreply.github.com>
2025-01-17 11:03:19 +0900

    [Bronze II] Title: 다이얼, Time: 104 ms, Memory: 14068 KB -BaekjoonHub

---
+++ 백준/Bronze/5622. 다이얼/다이얼.java
@@ -0,0 +1,31 @@
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+        int cnt = 0;
+        for (int i=0; i<s.length(); i++) {
+            char c = s.charAt(i);
+            if ((c=='A') || (c=='B') || (c=='C')) {
+                cnt+=3;
+            } else if ((c=='D') || (c=='E') || (c=='F')) {
+                cnt+=4;
+            } else if ((c=='G') || (c=='H') || (c=='I')) {
+                cnt+=5;
+            } else if ((c=='J') || (c=='K') || (c=='L')) {
+                cnt+=6;
+            } else if ((c=='M') || (c=='N') || (c=='O')) {
+                cnt+=7;
+            } else if ((c=='P') || (c=='Q') || (c=='R') || (c=='S')) {
+                cnt+=8;
+            } else if ((c=='T') || (c=='U') || (c=='V')) {
+                cnt+=9;
+            } else if ((c=='W') || (c=='X') || (c=='Y') || (c=='Z')) {
+                cnt+=10;
+            }
+        }
+        System.out.println(cnt);
+    }
+}
