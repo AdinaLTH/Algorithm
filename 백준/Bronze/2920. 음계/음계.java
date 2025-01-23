0b5bae20d9e7c4a4619846fe39cf7c06fde8ef6b
kawaiirockcrab <134618865+taehotaeho@users.noreply.github.com>
2025-01-23 12:42:58 +0900

    [Bronze II] Title: 음계, Time: 100 ms, Memory: 14084 KB -BaekjoonHub

---
+++ 백준/Bronze/2920. 음계/음계.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[] asc = {1,2,3,4,5,6,7,8};
+        int[] desc = {8,7,6,5,4,3,2,1};
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int[] arr = new int[8];
+        for (int i=0; i<8; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        if (Arrays.equals(arr, asc)) {
+            System.out.println("ascending");
+        } else if (Arrays.equals(arr, desc)) {
+            System.out.println("descending");
+        } else {
+            System.out.println("mixed");
+        }
+    }
+}
