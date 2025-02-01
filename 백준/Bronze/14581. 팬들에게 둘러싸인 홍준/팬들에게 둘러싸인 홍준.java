7fc1fac42db1b401cc33e2fbd3928a7f25015edb
kawaiirockcrab <134618865+taehotaeho@users.noreply.github.com>
2025-02-01 21:46:20 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 128 ms, Memory: 15868 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.java
@@ -0,0 +1,12 @@
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] arsgs) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String id = br.readLine();
+        System.out.println(":fan::fan::fan:");
+        System.out.println(":fan::"+id+"::fan:");
+        System.out.println(":fan::fan::fan:");
+    }
+}
