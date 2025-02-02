5a873566580bcecc1685ed4f4e0ac2f459093f3b
kawaiirockcrab <134618865+taehotaeho@users.noreply.github.com>
2025-02-02 10:13:08 +0900

    [Bronze V] Title: UCPC에서 가장 쉬운 문제 번호는?, Time: 100 ms, Memory: 14116 KB -BaekjoonHub

--- 백준/Bronze/25311. UCPC에서 가장 쉬운 문제 번호는？/UCPC에서 가장 쉬운 문제 번호는？.java
+++ 백준/Bronze/25311. UCPC에서 가장 쉬운 문제 번호는？/UCPC에서 가장 쉬운 문제 번호는？.java
@@ -4,7 +4,10 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int year = Integer.parseInt(br.readLine());
-        if (year==2018 || year==2019 || year==2020 || year==2021 || year==2022) System.out.println('A');
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        br.readLine();
+        bw.write('A');
+        br.close();
+        bw.close();
     }
 }
