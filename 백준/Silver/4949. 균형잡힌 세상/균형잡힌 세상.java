525418e40bdc610f297a08ad83f0b12044b5ed0f
kawaiirockcrab <134618865+taehotaeho@users.noreply.github.com>
2025-02-07 21:34:25 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 152 ms, Memory: 19332 KB -BaekjoonHub

---
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.java
@@ -0,0 +1,60 @@
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        String s;
+
+        while (true) {
+            s = br.readLine();
+
+            if (s.equals(".")) {
+                break;
+            }
+
+            sb.append(solve(s)).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+
+    private static String solve(String s) {
+        char[] stack = new char[s.length()];
+        int size = 0;
+
+        for (char val : s.toCharArray()) {
+
+            if (val == '(' || val == '[') {
+                stack[size] = val;
+                size++;
+            }
+            else if (val == ')') {
+
+                if (size == 0 || stack[size-1] != '(') {
+                    return "no";
+                }
+                else {
+                    size--;
+                }
+            }
+            else if (val == ']') {
+
+                if (size == 0 || stack[size-1] != '[') {
+                    return "no";
+                }
+                else {
+                    size--;
+                }
+            }
+        }
+
+        if (size != 0) {
+            return "no";
+        }
+        else {
+            return "yes";
+        }
+    }
+}
