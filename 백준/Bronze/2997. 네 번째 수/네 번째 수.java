d9c27222ec5ffdd96eee466de92f14a290c2d8dd
kawaiirockcrab <134618865+taehotaeho@users.noreply.github.com>
2025-03-09 12:43:37 +0900

    [Bronze III] Title: 네 번째 수, Time: 100 ms, Memory: 14128 KB -BaekjoonHub

---
+++ 백준/Bronze/2997. 네 번째 수/네 번째 수.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+        int max, min, mid = 0;
+
+        max = Math.max(a, b);
+        max = Math.max(max, c);
+        min = Math.min(a, b);
+        min = Math.min(min, c);
+
+        if (a>min && a<max) {
+            mid = a;
+        } else if (b>min && b<max) {
+            mid = b;
+        } else if (c>min && c<max) {
+            mid = c;
+        }
+
+        int dif = Math.min(mid - min, max - mid);
+        if (mid-min != max-mid) {
+            if (mid - min < max - mid) {
+                System.out.println(mid + dif);
+            } else if (mid - min > max - mid) {
+                System.out.println(mid - dif);
+            }
+        } else {
+            System.out.println(max+dif);
+        }
+    }
+}
